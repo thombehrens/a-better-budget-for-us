6be8eee455fef7ec6763e1383531369740aea67d
Jesse Jessee <jesse@jessejessee.com>
2021-02-01 21:47:04 -0700

    Created new module to handle groups/arrays of worker types allowing for their stats to be adjusted.

---
+++ src/components/WorkerTypeModuleCollection.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="worker-type-module">
+
+    <h2>{{ title }}</h2>
+    <p><i>{{ description }}</i></p>
+
+    <ul class="worker_type_stats">
+      <li class="worker_type" v-for="worker_type in worker_types" :key="worker_type.id">
+        <ul class="stats">
+          <li class="stat">
+            <label :for="worker_type.name + '_salary_input'">Average Salary: </label>
+            <input :id="worker_type.name + '_salary_input'" type="number" v-model="worker_type.salary">
+          </li>
+          <li class="stat">
+            <label :for="worker_type.name + '_benefits_input'">Average Benefits: </label>
+            <input :id="worker_type.name + '_cenefits_input'" type="number" v-model="worker_type.benefits">
+          </li>
+          <li class="stat">
+            <label :for="worker_type.count + '_count_input'">Number of Staff: </label>
+            <input :id="worker_type.count + '_counts_input'" type="number" v-model="worker_type.count">
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <!-- <div class="solution">
+        <p>Adjust percentage of exec salary cut</p>
+        <VueSlideBar :min="0" :max="100" v-model="worker_type.strategies.salary_cut"></VueSlideBar>
+    </div>
+
+    <div class="solution">
+        <p>Adjust percentage of exec benefits cut</p>
+        <VueSlideBar :min="0" :max="100" v-model="worker_type.strategies.benefit_cut"></VueSlideBar>
+    </div> -->
+
+  </div>
+</template>
+
+<script>
+
+// import VueSlideBar from 'vue-slide-bar'; 
+
+export default {
+  name: 'WorkerTypeModuleCollection',
+  components: {
+      // VueSlideBar
+  },
+  props: {
+    title: String,
+    description: String,
+    worker_types: Array
+  },
+  methods: {
+    //   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .solution {
+        background-color: #eee;
+        margin-bottom: 15px;
+        padding: 25px;
+    }
+    .solution p {
+        margin: 0px;
+    }
+    .worker_type_stats {
+      margin: 0px auto;
+      max-width: 400px;
+      text-align: center;
+    }
+    .worker_type_stats .worker_type {
+      display: block;
+    }
+    .worker_type_stats .stats {
+      margin-bottom: 25px;
+      text-align: left;
+    }
+    .worker_type_stats .stats .stat {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+</style>
