1df2b7fef768873ad4f4f5d4d6087d4e9df8a256
Jesse Jessee <jesse@jessejessee.com>
2021-02-03 22:09:00 -0700

    Added furlough feature.

--- src/App.vue
+++ src/App.vue
@@ -26,16 +26,27 @@
     :worker_types="getWorkerType('other')">
     </WorkerTypeModuleCollection>
 
+    <div id="staff-furlough">
+        <h2>How many unpaid furlough days should staff be required to take?</h2>
+        <VueSlideBar :min="0" :max="12" v-model="policies.furlough.num_days"></VueSlideBar>
+        <h3>Should staff be able to use vacation days instead of furlough days</h3>
+        <toggle-button v-model="policies.furlough.vacation_for_furlough" :labels="{checked: 'Yes', unchecked: 'No'}" :width="100" :height="50" :fontSize="18" />
+    </div>
+
   </div>
 </template>
 
 <script>
 
+import VueSlideBar from 'vue-slide-bar'; 
+import { ToggleButton } from 'vue-js-toggle-button'
 import WorkerTypeModuleCollection from './components/WorkerTypeModuleCollection.vue';
 
 export default {
   name: 'App',
   components: {
+    VueSlideBar,
+    ToggleButton,
     WorkerTypeModuleCollection
   },
   data() {
@@ -51,7 +62,12 @@ export default {
         work_days: 207
       },
       policies: {
-        vacation_for_furlough: 1.0
+        furlough: {
+          vacation_for_furlough: false,
+          num_days: 0,
+          can_take_vacation_percentage: 0.7,
+          cannot_take_vacation_percentage: 1
+        }
       },
       workers: {
         types: [
